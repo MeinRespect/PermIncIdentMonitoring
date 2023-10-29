45fcb1ecbdaf58d462a0c7dad7b1d85d4da44801
Denikaev Emil <edenikaev@mail.ru>
2023-10-29 04:11:12 +0400

    project

---
+++ frontend/perm/src/store/getters/getters.js
@@ -0,0 +1,14 @@
+export default {
+  GRAPH(state) {
+    return state.graph;
+  },
+  PREDICT(state) {
+    return state.predict;
+  },
+  INFO(state) {
+    return state.info;
+  },
+  ALLINFO(state) {
+    return state.allinfo;
+  },
+};
